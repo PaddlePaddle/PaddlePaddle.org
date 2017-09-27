75822d3a610cfeaa8faf9c6a5e9949e11214676c
ludaming <ludaming@baidu.com>
2017-09-27 11:59:29 -0700

    book csf generator

--- portal/portal/sitemap_helper.py
+++ portal/portal/sitemap_helper.py
@@ -1,75 +1,69 @@
+import json
+import os
+import re
 from django.conf import settings
+from stat import *
 
 
 def get_tutorial_links():
-    """
-    1) call _get_root_csf()
-    2) using root CSF to generate link map for Tutorial page
-
-    Tutorial == book
-    """
-    pass
-
-
-def _get_root_csf():
-    """
-    1) determine if root content schematic file (CSF) exists in content directory
-    2) if root CSF does not exist or is out of date, generate CSF for each content directory (doc, blog, book, models)
-    3) once each CSF is built, merge them to form root CSF and save them in content directory
-    4) if no errors, return root csf, otherwise throw exception
-    """
-    pass
+    obj = SiteMapCSFAdapter()
+    obj.parse()
 
 
 class ContentSchematicFile():
-    def __init__(self):
-        self.file_path = settings.EXTERNAL_TEMPLATE_DIR + 'root_csf.yaml'
-        self.csf_data = None  # dict
+    @staticmethod
+    def iterate_folder(top, paths, is_chinese=False):
+        for f in os.listdir(top):
+            pathname = os.path.join(top, f)
+            mode = os.stat(pathname)[ST_MODE]
+            if S_ISDIR(mode):
+                ContentSchematicFile.iterate_folder(pathname, paths, is_chinese=is_chinese)
+            elif S_ISREG(mode):
+                pieces = pathname.split('.')
+                if pieces[-1] == 'html':
+                    if is_chinese:
+                        matchObj = re.match(r'^.*(\d{2})\.(.*)/.*cn\.html$', pathname)
+                    else:
+                        matchObj = re.match(r'^.*(\d{2})\.(.*)/.*index\.html$', pathname)
 
-    def is_valid(self):
-        """
-        check if csf exists and not expired
-        """
-        pass
+                    if matchObj:
+                        title = ' '.join(matchObj.group(2).split('_'))
+                        link = pathname[len(settings.EXTERNAL_TEMPLATE_DIR):]
+                        paths.append({
+                            "title": title,
+                            "link": link
+                        })
+            else:
+                continue
 
     @staticmethod
-    def get_site_csf():
-        """
-        returns a csf if it exists and valid, otherwise generate the root csf and returns it when complete
-        throws exceptions once there are errors
-
-        return ContentSchematicFile.generate_csf( SiteMapCSFAdapter() )
-        """
-        pass
-
-    @staticmethod
-    def generate_csf(csf_adapter=None):
+    def generate_book_csf(site_dir, id="", title=""):
         """
         generate csf
         """
-        pass
+        if os.path.exists(site_dir + '/site.json') and os.path.exists(site_dir + '/site_cn.json'):
+            return
+
+        sections = []
+        ContentSchematicFile.iterate_folder(site_dir, sections, is_chinese=False)
+
+        cur_csf = {"id": id, "title": title, "sections": sections}
+
+        with open(site_dir + '/site.json', 'w') as fp:
+            fp.write(json.dumps(cur_csf))
+
+        sections = []
+        ContentSchematicFile.iterate_folder(site_dir, sections, is_chinese=True)
+
+        cur_csf = {"id": id, "title": title, "sections": sections}
+
+        with open(site_dir + '/site_cn.json', 'w') as fp:
+            fp.write(json.dumps(cur_csf))
+
+        return cur_csf
 
 
-class CSFAdapter:
-    def __init__(self):
-        pass
-
-    def parse(self):
-        raise NotImplementedError("Should have implemented this")
-
-
-class DirectoryCSFAdapter(CSFAdapter):
-    def __init__(self, dir):
-        self.dir = dir
-
-    def parse(self):
-        """
-        go through files in dir and generate CSF.json for each content directory
-        """
-        pass
-
-
-class SiteMapCSFAdapter(CSFAdapter):
+class SiteMapCSFAdapter():
     def parse(self):
         """
         combine content dir CSFs into a root CSF
@@ -80,4 +74,8 @@ class SiteMapCSFAdapter(CSFAdapter):
 
         // TODO: merge blogCFS && documentCFS to create root CSF and return
         """
-        pass
+        bookCFS = ContentSchematicFile.generate_book_csf(
+            settings.EXTERNAL_TEMPLATE_DIR + '/book',
+            id="tutorial",
+            title="Deep Learning 101"
+        )
