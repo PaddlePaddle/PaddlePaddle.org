214f64d426660a5e2ed817fec019ecbc0ee6c339
cs2be <nguyenthuan@baidu.com>
2017-10-16 23:25:04 -0700

    update code to allow directly linking to h1 and h2 (instead of requiring the headers to have permalinks)

--- portal/portal/static/js/permalink-nav.js
+++ portal/portal/static/js/permalink-nav.js
@@ -9,34 +9,45 @@ var PermalinkNav = {
         navContainer = $(navContainerSelector);
 
         if (navContainer.length) {
-            permalinks = $("h1:has(a),h2:has(a)");
+            permalinks = $("h1,h2");
 
             if (permalinks.length == 0) {
                 navContainer.hide()
             } else {
+                var maxIdLength = 30;
+
                 $(".doc-content").addClass('with-permalink-nav');
 
                 var containerOl;
                 permalinks.each(function(index) {
                     var header = $(this);
 
+                    var headerId = header.attr('id');
+                    var headerText = header.immediateText();
+
+                    if (!headerId) {
+                        // Create a permalink id on header (if header does not have an id already)
+                        headerId = ("permalink-" + headerText.replace(/\W+/g, "-")).toLowerCase();
+                        if (headerId > maxIdLength) {
+                            headerId = headerId.substring(0, maxIdLength);
+                        }
+                        header.attr("id", headerId);
+                    }
+
                     if (header.is("h1")) {
                         containerOl = $("<ol/>");
-
-                        var link = header.find("a:first").attr("href")
-                        var linkElement = $('<a />', { text: header.immediateText(), href: link })
+                        var link = "#" + headerId;
+                        var linkElement = $('<a />', { text: headerText, href: link })
                         containerOl.append(linkElement);
                         navContainer.append(containerOl);
-
                     } else if (header.is("h2")) {
                         if (containerOl == null) {
                             containerOl = $("<ol/>");
                             navContainer.append(containerOl);
                         }
 
-                        var link = header.find("a:first").attr("href")
-
-                        var linkElement = $('<a />', { text: header.immediateText(), href: link })
+                        var link = "#" + headerId;
+                        var linkElement = $('<a />', { text: headerText, href: link })
                         var containerLi = $("<li/>");
                         containerLi.append(linkElement);
                         containerOl.append(containerLi);
@@ -44,7 +55,7 @@ var PermalinkNav = {
                 });
 
                 var offset = 0;
-                var variance = 4;   // Permalinks are slightly below the header content, so we have to adjust for it
+                var variance = 30;   // Permalinks are slightly below the header content, so we have to adjust for it
 
                 navContainer.find('a[href^="#"]').click(function(event) {
                     // Prevent from default action to intitiate
