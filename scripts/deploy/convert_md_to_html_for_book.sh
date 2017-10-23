92641659feb78ebd15d478a7a96ad5f0962c6ec6
daming-lu <daminglu@yahoo.com>
2017-10-23 16:24:56 -0700

    test new script

---
+++ scripts/deploy/convert_md_to_html_for_book.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEPLOY_DOCS_DIR=$2
+
+echo "convert md to html for book to $SOURCE_DIR => $DEPLOY_DOCS_DIR"
+
+
+
+
--- scripts/deploy/deploy_docs.sh
+++ scripts/deploy/deploy_docs.sh
@@ -2,8 +2,9 @@
 set -e
 
 DEC_PASSWD=$1
-GITHUB_BRANCH=$2
-SOURCE_DIR=$3
+SOURCE_DIR=$2
+GITHUB_BRANCH=$3 # version
+
 
 echo "1:($1) 2:($2) 3:($3)"
 
@@ -21,37 +22,46 @@ mkdir $DEPLOY_DOCS_DIR
 # TODO:  Call HTML generation code for book, model, and Paddle/doc.  Copy generated HTML
 # to .ppo_workspace/generated_docs
 
-$GENERATED_DOCS_DIR= #TODO: Set GENERATED_DOCS DIR: ie: /.ppo_workspace/generated_docs/book
-
-### pull PaddlePaddle.org app and run the deploy_documentation command
-# https://github.com/PaddlePaddle/PaddlePaddle.org/archive/develop.zip
-
-curl -LOk https://github.com/PaddlePaddle/PaddlePaddle.org/archive/develop.zip
-
-unzip develop.zip
-
-cd PaddlePaddle.org-develop/
-
-cd portal/
-
-sudo pip install -r requirements.txt
-
-mkdir ./tmp
-python manage.py deploy_documentation $SOURCE_DIR $GENERATED_DOCS_DIR $GITHUB_BRANCH
-
-
-# deploy to remote server
-openssl aes-256-cbc -d -a -in ../scripts/deploy/content_mgr.pem.enc -out content_mgr.pem -k $DEC_PASSWD
-
-
-eval "$(ssh-agent -s)"
-chmod 400 content_mgr.pem
-
-
-ssh-add content_mgr.pem
-rsync -r $DEPLOY_DOCS_DIR/content/docs content_mgr@52.76.173.135:/var/content/docs
-
-
-chmod 644 content_mgr.pem
-
-rm -rf $DEPLOY_DOCS_DIR
+if [[ $SOURCE_DIR == *"book"* ]]; then
+  echo "It is book repo"
+  sh convert_md_to_html_for_book.sh $SOURCE_DIR $DEPLOY_DOCS_DIR
+fi
+#
+#
+#$GENERATED_DOCS_DIR= #TODO: Set GENERATED_DOCS DIR: ie: /.ppo_workspace/generated_docs/book
+#
+#### pull PaddlePaddle.org app and run the deploy_documentation command
+## https://github.com/PaddlePaddle/PaddlePaddle.org/archive/develop.zip
+#
+#PPO_BRANCH=move_book_conversion_logic_to_PPO_script
+#
+#
+#curl -LOk https://github.com/PaddlePaddle/PaddlePaddle.org/archive/$PPO_BRANCH.zip
+#
+#unzip $PPO_BRANCH.zip
+#
+#cd PaddlePaddle.org-$PPO_BRANCH/
+#
+#cd portal/
+#
+#sudo pip install -r requirements.txt
+#
+#mkdir ./tmp
+#python manage.py deploy_documentation $SOURCE_DIR $GENERATED_DOCS_DIR $GITHUB_BRANCH
+#
+#
+## deploy to remote server
+#openssl aes-256-cbc -d -a -in ../scripts/deploy/content_mgr.pem.enc -out content_mgr.pem -k $DEC_PASSWD
+#
+#
+#eval "$(ssh-agent -s)"
+#chmod 400 content_mgr.pem
+#
+#
+#ssh-add content_mgr.pem
+#rsync -r $DEPLOY_DOCS_DIR/content/docs content_mgr@52.76.173.135:/var/content/docs
+#
+#
+#chmod 644 content_mgr.pem
+#
+#rm -rf $DEPLOY_DOCS_DIR
