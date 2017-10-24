268f0ccbe9ac9385d79069d9c2e7a5f39195b74c
Wang,Jeff <wangjeff@baidu.com>
2017-10-24 12:03:01 -0700

    Fix the incorrect path issue.

--- portal/deploy/sitemap_generator.py
+++ portal/deploy/sitemap_generator.py
@@ -16,11 +16,11 @@ def models_sitemap(original_documentation_dir, generated_documentation_dir, vers
 
     # Create models sitemap template
     sections = []
-    sitemap = {"title": {"zh": "models"}, 'sections': sections}
+    sitemap = {"title": {"zh": "概述"}, 'sections': sections}
 
     # Read the stripped html file
     # TODO [Jeff Wang]: Confirm the root_html_path is correct
-    root_html_path = os.path.join(generated_documentation_dir, 'README.html')
+    root_html_path = os.path.join(generated_documentation_dir, 'README.cn.html')
     with open(root_html_path) as original_html_file:
         soup = BeautifulSoup(original_html_file, 'lxml')
 
@@ -28,6 +28,7 @@ def models_sitemap(original_documentation_dir, generated_documentation_dir, vers
         # Extract the links and the article titles
         for tag in anchor_tags:
             title = {'zh': tag.text}
+            # The absolute URLs link to the github site. Transform them into relative URL for local HTML files.
             link_zh = tag['href'].replace(github_path, '') + '/README.html'
             link = {'zh': link_zh}
 
@@ -36,10 +37,10 @@ def models_sitemap(original_documentation_dir, generated_documentation_dir, vers
 
 
     # TODO [Jeff Wang]: Confirm the models sitemap path is correct
-    versioned_dest_dir = destination_documentation_dir + '/' + version
+    versioned_dest_dir = destination_documentation_dir + '/' + version + '/models'
     if not os.path.isdir(versioned_dest_dir):
         os.mkdir(versioned_dest_dir)
-    sitemap_path = os.path.join(versioned_dest_dir, 'models.json')
+    sitemap_path = os.path.join(versioned_dest_dir, 'site.json')
     # Update the models.json
     with open(sitemap_path, 'w') as outfile:
         json.dump(sitemap, outfile)
